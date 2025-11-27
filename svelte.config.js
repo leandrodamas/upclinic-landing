96d4b89cbe1fa3c4d3b9ef37a626d2cc3b35c1ac
Seu Nome <seu@email.com>
2025-11-27 16:02:26 -0300

    fix: migrate to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
+export default {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    }),
-    prerender: {
-      entries: ['*']
-    }
-  }
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
-
-export default config;
-
