4afce61a2280aca93624c20032e54b030d188ef1
leandrodamas <leandrodamas@hotmail.com>
2025-11-28 14:21:37 -0300

    Change Svelte adapter from static to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    }),
-    prerender: {
-      entries: ['*']
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter(),
+        prerender: {
+            entries: ['*']
+        }
     }
-  }
 };
 
 export default config;
