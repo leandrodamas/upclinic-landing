e1327d358cb7699a8ea3c1ac829ac1307096e43d
leandrodamas <leandrodamas@hotmail.com>
2025-11-29 19:38:54 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
-        }),
-
-        prerender: {
-            entries: ['*']
-        }
-    }
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			entries: ['*']
+		}
+	}
 };
 
 export default config;
