4ba5e81482e9ab2943c7354f1ff8d85038f59adf
Seu Nome <seu@email.com>
2025-11-29 22:26:35 -0300

    Fix: usar import direto de logos com Vite para garantir funcionamento no Vercel

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
@@ -248,7 +248,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "eb86t1"
+  version_hash: "1azimny"
 };
 async function get_hooks() {
   let handle;
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.B63xOhCP.js","_app/immutable/chunks/nKQ1bpcA.js","_app/immutable/chunks/D31EXCKY.js","_app/immutable/chunks/CCBigHkd.js"];
+export const imports = ["_app/immutable/nodes/1.DJW0y_r8.js","_app/immutable/chunks/nKQ1bpcA.js","_app/immutable/chunks/D31EXCKY.js","_app/immutable/chunks/CGdOIqpl.js"];
 export const stylesheets = [];
 export const fonts = [];
