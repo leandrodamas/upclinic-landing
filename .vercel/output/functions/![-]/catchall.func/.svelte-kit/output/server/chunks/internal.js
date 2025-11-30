4d7273c6a21e8b185866522556d1f8c4410cdeff
Seu Nome <seu@email.com>
2025-11-30 00:02:42 -0300

    fix: deploy final - remove all logo files and references - 2025-11-30 00:02:42

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
@@ -248,7 +248,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1ohcbsi"
+  version_hash: "1ik1fde"
 };
 async function get_hooks() {
   let handle;
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CqFmqEvk.js","_app/immutable/chunks/CtICX558.js","_app/immutable/chunks/TvkdS4Ko.js","_app/immutable/chunks/dfMCJz4S.js"];
+export const imports = ["_app/immutable/nodes/1.OL5DzNbk.js","_app/immutable/chunks/CtICX558.js","_app/immutable/chunks/TvkdS4Ko.js","_app/immutable/chunks/DRYyTI92.js"];
 export const stylesheets = [];
 export const fonts = [];
